c272e60cf92452ed4a031f851d8bf342fd046967
naiithink <80931264+naiithink@users.noreply.github.com>
2021-10-01 09:22:14 +0700

    Updated.

---
+++ y1/CS112-LEC/LEC03.py
@@ -0,0 +1,10 @@
+#print('first line')
+#print('2')             #hidden end='\n'
+#print('3')             #hidden end='\n'
+#print('4', '\n')
+#print('last line')
+
+#test1 = '{} {}'.format('hello', 'world')
+#test2 = '%s %s %s' % ('hello', 'world', '(again)')
+#print(test1)
+#print(test2)
---
+++ y1/CS112-LEC/LEC11.py
@@ -0,0 +1,7 @@
+"""
+ls1 = list(range(-1:-5:-1))
+"""
+# %%
+[x, y, z] = [input(), input(), input()]
+print(x, z, y)
+# %%
