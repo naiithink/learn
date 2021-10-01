7f87f297836cd3351ef017ab57e095fd0cbdaccd
naiithink <80931264+naiithink@users.noreply.github.com>
2021-10-01 09:22:14 +0700

    Updated.

---
+++ y1/CS112-LEC/LEC04.py
@@ -0,0 +1,5 @@
+x = 5
+y = int(input('Please enter a number: '))
+if y >= 0:
+    print('The result is {:3d}.'.format(x+y))
+print('Sorry, you must enter the number which greater than 0.')
---
+++ y1/CS112-LEC/LEC06.py
@@ -0,0 +1,39 @@
+#01
+'''
+string = input('Word: ')
+count = 0
+times = int(input())
+while count < times:
+    count += 1
+    print(string, count)
+'''
+
+#02
+'''
+n = 1
+while n <= 5:
+    print(n)
+    n += 1
+'''
+
+#03
+'''
+    n = 1
+while n <= 5:
+    print(n)
+    n += 1
+'''
+'''
+x = 1
+total = x
+count = 0
+while count < 5:
+    x += 1
+    count += 1
+    total += x
+
+print(total)
+'''
+
+x = 2
+print(2**64)
