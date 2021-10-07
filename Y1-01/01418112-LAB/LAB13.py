156293f907ba4ae229cb5bc7e64f161193b18dfb
naiithink <80931264+naiithink@users.noreply.github.com>
2021-10-07 17:53:45 +0700

    LAB13

--- Y1-01/01418112-LAB/LAB13.py
+++ Y1-01/01418112-LAB/LAB13.py
@@ -1,19 +1,24 @@
 def namelist(names):
     product = ""
-    if names:
-        name = len(names) - 1
-        indx = 0
-        brk = False
-        j = 0
-        for i in names:
-            sep = [", ", " & "]; product += i
-            if not brk:
-                if indx == name - 1:
-                    j += 1
-                product += sep[j]
-                if sep[j] == " & ":
-                    brk = True
-            indx += 1
+    if len(names) == 1:
+        product += names[0]
+    else:
+        if names:
+            name = len(names) - 1
+            indx = 0
+            brk = False
+            j = 0
+            for i in names:
+                sep = [", ", " & "]; product += i
+                if not brk:
+                    if indx == name - 1:
+                        j += 1
+                    product += sep[j]
+                    if sep[j] == " & ":
+                        brk = True
+                indx += 1
     return product
 
-print( namelist(['Bart', 'Viola', 'Peter', 'Nostel']) )
+print( namelist(['Bart']) )
+
+def 
