c8c402342905229b7729decc9960b755d16ded04
naiithink <potsawattkw@outlook.com>
2021-10-18 02:22:43 +0700

    Add & Edit 112-LEC Final ASSGs

---
+++ Y1-01/01418112-LEC/FinalAssignment/6410451199-10.py
@@ -0,0 +1,34 @@
+def sumRowCol(ls, numr, numc):
+    iPut = 0; smr = 0; sumr = []; sumc = []
+    while iPut < numc:
+        sumc.append(0)
+        iPut += 1
+    for i in ls:
+        cnt = 0
+        for j in i:
+            col = cnt % numc
+            smr += int(j)
+            cnt += 1
+            if cnt == numc:
+                sumr.append(smr)
+                smr = 0
+            sumc[col] += int(j)
+    print(sumr, sumc, sep="\n")
+
+numr = int(input())
+numc = int(input())
+ls = []; cnt = 0
+while cnt < numr:
+    ls.append(input().split(" "))
+    cnt += 1
+sumRowCol(ls, numr, numc)
+
+"""
+5
+6
+0 -1 3 8 6 7
+2 2 2 2 2 2
+3 4 1 9 2 -3
+-2 8 10 12 5 1
+8 2 9 3 4 7
+"""
---
+++ Y1-01/01418112-LEC/FinalAssignment/6410451199-11.py
@@ -0,0 +1,17 @@
+def firstClosestPair(ls):
+    ls = ls.split(" ")
+    cnt = 0
+    dif = {}
+    stopAt = len(ls) - 1
+    while cnt < stopAt:
+        dif[f"{cnt} {cnt + 1}"] = abs(float(ls[cnt]) - float(ls[cnt + 1]))
+        cnt += 1
+    dis = min(list(dif.values()))
+    pair = [key for key, value in dif.items() if value == dis]
+    position = pair[0].split(" ")
+    print(ls[int(position[0])], ls[int(position[1])])
+
+ls = input()
+firstClosestPair(ls)
+
+"50 61 108 123 101 91 98 0"
