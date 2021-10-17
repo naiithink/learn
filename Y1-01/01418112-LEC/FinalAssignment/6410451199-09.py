e110e76aeb2d117c38463e93f7a2441820e6e79e
naiithink <potsawattkw@outlook.com>
2021-10-18 00:20:59 +0700

    Update 112-LEC 09

--- Y1-01/01418112-LEC/FinalAssignment/6410451199-09.py
+++ Y1-01/01418112-LEC/FinalAssignment/6410451199-09.py
@@ -1,3 +1,12 @@
 def shiftElement(ls, n, direction):
+    if ls and n and direction:
+        if direction == "r":
+            product = ("0 " * n) + ls[n:]
+        elif direction == "l":
+            product = ls[:- (n + 1)] + (" 0" * n)
+        return product
 
-ls = 
+ls = "a 12.5 30 dog cat student 498"
+n = 3
+direction = "l"
+print(shiftElement(ls, n, direction))
