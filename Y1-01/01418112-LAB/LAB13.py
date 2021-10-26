2d29c519de780d8fb5f6b9306b765b57cf46a8e4
naiithink <potsawattkw@outlook.com>
2021-10-26 10:21:01 +0700

    112-LAB LAB13

--- Y1-01/01418112-LAB/LAB13.py
+++ Y1-01/01418112-LAB/LAB13.py
@@ -1,28 +1,20 @@
-"""def namelist(names):
-    product = ""
-    if len(names) == 1:
-        product += names[0]
-    else:
-        if names:
-            name = len(names) - 1
-            indx = 0
-            brk = False
-            j = 0
-            for i in names:
-                sep = [", ", " & "]; product += i
-                if not brk:
-                    if indx == name - 1:
-                        j += 1
-                    product += sep[j]
-                    if sep[j] == " & ":
-                        brk = True
-                indx += 1
-    return product
+daysInAMonth = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
 
-print( namelist(['Bart']) )
-"""
-
-coor = [input().split(","), input().split(",")]
-coor = [float(j.strip()) for i in coor for j in i]
-coor = [coor[0] + coor[2], coor[1] + coor[3]]
-print(*coor, sep=",")
+inputDate = [input().split("-")[::-1], input().split("-")[::-1]]
+print(inputDate)
+if inputDate:
+    if inputDate[0][0] == inputDate[1][0]:
+        
