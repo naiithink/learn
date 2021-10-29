5cd3870f8ecaf6cdc42d07704a8948df68e4cf8a
naiithink <potsawattkw@outlook.com>
2021-10-29 16:01:45 +0700

    Complete LAB13, LAB14

--- Y1-01/01418112-LAB/LAB13.py
+++ Y1-01/01418112-LAB/LAB13.py
@@ -1,20 +1,42 @@
 daysInAMonth = {
-    1: 31,
-    2: 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31
+    31: [1, 3, 5, 7, 8, 10, 12],
+    30: [4, 6, 9, 11],
+    28: [2]
 }
 
-inputDate = [input().split("-")[::-1], input().split("-")[::-1]]
-print(inputDate)
-if inputDate:
-    if inputDate[0][0] == inputDate[1][0]:
-        
+def generate_yearMap(daysInAMonth, yearMap={}, initDay=1):
+    for month in range(1, 13):
+        max = [days for days in daysInAMonth if month in daysInAMonth[days]]
+        max[0] += initDay
+        yearMap[month] = list(range(initDay, max[0]))
+        initDay = yearMap[month][-1] + 1
+    return yearMap
+
+yearMap = generate_yearMap(daysInAMonth)
+def invalidFound(inputDate):
+    invalid = False
+    for month in dateInput:
+        if month not in range(1, 13):
+            invalid = 'Wrong Month'
+        else:
+            for day in dateInput[month]:
+                if day not in daysInAMonth[month]:
+                    invalid = 'Wrong Day'
+    return invalid
+
+dateInput = []
+while len(dateInput) < 3:
+    if len(dateInput) < 2:
+        inputDate = input().split('-')
+    else:
+        inputDate = [input(), '1']
+    if not invalidFound(inputDate):
+        dateInput.append([int(i) for i in inputDate])
+
+if invalidFound(inputDate):
+    print(invalidFound(inputDate))
+else:
+    if dateInput[0][1] == dateInput[1][1]:
+        dateInput[int(dateInput[1][1])] = sorted([int(dateInput[0][0]), int(dateInput[1][0])])
+
+print('eiei', dateInput)
