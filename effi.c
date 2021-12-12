d57c70edcca745c5699d0e5f1351e3e71cb93ac1
naiithink <potsawattkw@outlook.com>
2021-12-13 00:00:40 +0700

    update shell

---
+++ effi.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+
+typedef struct
+{
+    char *os;
+    char *arch;
+    enum {os, arch, END} *error;
+}
+platform;
+
+int main(void)
+{
+    clock_t start, end;
+    platform *ison;
+
+    ison->error = "eiei";
+    ison->os = "mac";
+
+    start = clock();
+
+    if (ison->os == NULL || ison->arch == NULL)
+    {
+        for (int i = os; i < END; i++)
+        {
+            ison->error = i;
+            if ()
+            ison->error = ;
+        }
+    }
+
+    if (ison->error != NULL)
+    {
+        printf("error is not null\n");
+        printf("%s\n", ison->error);
+    }
+    else
+    {
+        printf("error is NULL\n");
+    }
+
+    end = clock();
+    double elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("%f\n", elapsed);
+
+    return 0;
+}
