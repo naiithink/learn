0b548cf8c72c09550c417d62a1a4eabe6c4e780b
naiithink <potsawattkw@outlook.com>
2021-12-13 03:52:26 +0700

    org --all

--- lambda/2021-11-30/fi.py
+++
@@ -1,9 +0,0 @@
-import math
-
-n = 0
-fi = ((5 ** 0.5) + 1) / 2
-a = float(input(">>> "))
-
-while math.floor(a) > 0:
-    a = a / fi
-    print("#" * math.floor(a))
--- markup-langs/html/responsive-design/index.rst
+++
@@ -1,4 +0,0 @@
-.. toctree::
-    :caption: Responsive Design
-    
-    
