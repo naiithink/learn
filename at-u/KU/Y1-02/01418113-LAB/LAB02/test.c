a592e16a81a5beea1b92b16ad6507d9fe89b68f1
naiithink <potsawattkw@outlook.com>
2021-12-15 17:07:57 +0700

    in-class /at-u/KU/Y1-02/01418113-LAB/LAB02/*

--- at-u/KU/Y1-02/01418113-LAB/LAB02/test.c
+++ at-u/KU/Y1-02/01418113-LAB/LAB02/test.c
@@ -3,33 +3,59 @@
 
 int main(void)
 {
+    // char str[10];
 
-    printf("%li\n", sizeof(double));
-    // char in[2*sizeof(float)];
-    // float salary;
+    // gets(str);
+    // fgets(str, 10, stdin);
+
+    // printf("in - %s\n", str);
 
     /*
-    fgets(in, 2*sizeof(float), stdin);
-    salary = atof(in);
+    char str_i[10];
+    char str_f[10];
+    char str_l[10];
 
-    printf("%f\n", salary);
+    printf("Enter I: ");
+    fgets(str_i, 10, stdin);
+    printf("Enter F: ");
+    fgets(str_f, 10, stdin);
+    printf("Enter L: ");
+    fgets(str_l, 10, stdin);
 
-    printf("%lf\n", 200.00 * 100.00);
+    int i = atoi(str_i);
+    float f = atof(str_f);
+    long l = atol(str_l);
 
-    int a = 499999.99;
-    float b = 500000.0;
-    float c = 500000.1;
-    if (a == b)
-    {
-        printf("yes\n");
-        if (b == c)
-        {
-            printf("also\n");
-        }
-    }
-    if (b == c)
-    {
-        printf("also\n");
-    }
+    printf("%d - %.2f - %ld\n", i, f, l);
     */
+
+    // Boolean false
+    int found = 0;
+    float f2 = 0.0;
+    char c = '\0';              // terminate charater, null character, ascii code = 0
+
+    // "kong" = ['k', 'o', 'n', 'g', '\0']              // array of charaters
+
+    // Boolean true
+    char str[10] = "";
+    fgets(str, 10, stdin);
+    int i = atoi(str);
+
+    // Expressions
+    // Relational Operators:    > >= < <= == !=
+    // Logical Operators:       && || !
+    if (i < 500)
+    {
+        printf("if %d\n", i < 500);
+    }
+    else if (i >= 500 && i < 1000)
+    {
+        printf("else if %d\n", i >= 500 && i < 1000);
+    }
+    else
+    {
+        printf("else %d - %d\n", i < 500, i >= 500 && i < 1000);
+    }
+
+    return 0;
 }
