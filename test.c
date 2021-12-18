62c0ae4ef9817a11fd1254c54a5917b91afc9cf5
naiithink <potsawattkw@outlook.com>
2021-12-18 20:37:15 +0700

    try yep-test-flight

--- test.c
+++ test.c
@@ -3,5 +3,17 @@
 int main(void)
 {
     printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
+    printf("hello, world\n");
     return 0;
 }
