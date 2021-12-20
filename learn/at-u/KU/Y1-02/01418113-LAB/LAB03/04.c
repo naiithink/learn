411c978973585b7e0e39409f1bb6b2a6c1a858fc
naiithink <potsawattkw@outlook.com>
2021-12-20 21:34:24 +0700

    touch */01418113-LAB/LAB03/LAB03.ipynb

--- learn/at-u/KU/Y1-02/01418113-LAB/LAB03/04.c
+++ learn/at-u/KU/Y1-02/01418113-LAB/LAB03/04.c
@@ -4,18 +4,20 @@
 
 int fibo(int n)
 {
-    int x = (pow((1 + sqrt(5)), n) - pow((1 -sqrt(5)), n)) / (pow(2,n) * (sqrt(5)));
+    int x = ( (pow((1 + sqrt(5)), n)) - (pow((1 - sqrt(5)), n)) ) / (pow(2, n) * (sqrt(5)));
     return x;
 }
 
 void main()
 {
     char input_n[5];
-    fgets(input_n, 5,stdin);
-    int n, i;
+    int n;
+
+    fgets(input_n, 5, stdin);
     n = atoi(input_n);
-    for (i = 0; i <= n; i+=1)
+
+    for (int i = 0; i < n+1; i++)
     {
-        printf("F(%d) = %d\n",i,fibo(i));
+        printf("F(%d) = %d\n", i, fibo(i));
     }
 }
