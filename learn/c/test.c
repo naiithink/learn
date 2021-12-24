95a1946f792fdd9a300f631b7f6e926c594e5955
naiithink <potsawattkw@outlook.com>
2021-12-24 18:50:29 +0700

    update

--- learn/c/test.c
+++ learn/c/test.c
@@ -1,17 +1,25 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+// #include <stdarg.h>
 #include <sys/dir.h>
 #include <sys/param.h>
 #include <sys/types.h>
-#include <time.h>
+// #include <time.h>
 #include <unistd.h>
 
 // #include "yep.h"
 
+typedef enum {false, true} bool;
+
+static int exit_status;
+
 int
 main (int argc, char **argv)
 {
+    bool ok;
+
     
+
+    return exit_status = ok ? EXIT_SUCCESS : EXIT_FAILURE;
 }
---
+++ playgrounds/yep/sub-yep/yep-wheel_setup.md
@@ -0,0 +1,11 @@
+# yep-wheel Setup
+
+## **ENV**
+
+| ENV                | Description        |
+| :-                 | :-                 |
+| `YEP_PATH`         | Search path for yep-based apps. |
+
+## **Setting Up**
+
+1. set `YEP_PATH`.
