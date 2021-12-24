53088c4b626627df858c76c5d605390859943807
naiithink <potsawattkw@outlook.com>
2021-12-24 20:56:38 +0700

    start hackathai

--- playgrounds/yep/sub-yep/yep-wheel.c
+++ playgrounds/yep/sub-yep/yep-wheel.c
@@ -1,11 +1,11 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+// #include <stdarg.h>
 #include <sys/dir.h>
 #include <sys/param.h>
 #include <sys/types.h>
-#include <time.h>
+// #include <time.h>
 #include <unistd.h>
 
 // #include "yep.h"
@@ -13,5 +13,18 @@
 int
 main (int argc, char **argv)
 {
+    if (argc == 1)
+        exit(EXIT_FAILURE);
+
+    int fd[3];
+
+    if ( (pipe(fd)) < 0 )
+        exit(EXIT_FAILURE);
+
     
+
+    write (fd[1], argv[1], sizeof(argv[1]));
+    write (fd[1], argv[2], sizeof(argv[2]));
+    write (fd[1], argv[3], sizeof(argv[3]));
+
 }
