07b19475c97f4b894dc2a77ed51aa09f8d625d6d
naiithink <potsawattkw@outlook.com>
2021-12-24 20:56:38 +0700

    start hackathai

---
+++ learn/hackathai/01/test.js
@@ -0,0 +1,2 @@
+console.log("hello, world");
+const a = { test: "foo" };
---
+++ learn/hackathai/02/index.js
@@ -0,0 +1,5 @@
+console.log("hello, world", "cat");
+
+console.error("hello, world, error")
+
+console.warn("hello, world, warning")
