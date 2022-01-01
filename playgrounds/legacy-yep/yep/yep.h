ce400f4c709d22ec0c8edd112bd3a29381fd6af9
naiithink <potsawattkw@outlook.com>
2022-01-02 00:52:33 +0700

    org yep

---
+++ playgrounds/legacy-yep/yep/yep.h
@@ -0,0 +1,75 @@
+#define YEP_HEADER_VERSION "0.1"
+
+#include <stdlib.h>
+
+// Unixes: unix __unix__ __unix
+// MingGW: WIN32 _WIN32 __WIN32 __WIN32__ __MINGW32__ WINNT __WINNT __WINNT__ _X86_ i386 __i386
+
+#if (   defined __GNUC__                        \
+)
+#define YEP_R_C 1
+#elif ( defined __GNUG__                        || \
+        defined __clang__                       \
+)
+#define YEP_R_CPP 1
+#endif
+
+// ~/lists/predefined_macros.json
+#if (defined __linux__)
+#define YEP_IS_IN "linux"
+#elif ( defined __APPLE__ && defined __MACH__   || \
+        defined Macintosh                       || \
+        defined macintosh                       \
+)
+#define YEP_IS_IN "darwin"
+#elif ( defined __WIN64                         || \
+        defined __WIN32                         || \
+        defined __WIN16                         || \
+        defined __WINDOWS__                     \
+)
+#define YEP_IS_IN "win32"
+#endif
+
+#if (   defined __amd64__                       || \
+        defined __amd64                         || \
+        defined __x86_64__                      || \
+        defined __x86_64                        || \
+        defined i386                            || \
+        defined __i386                          || \
+        defined __i386__                        || \
+        defined _X86_                           || \
+        defined _M_IX86                         || \
+        defined _M_I86                          \
+)
+#define YEP_IS_ON "amd64"
+#elif ( defined __aarch64__                     || \
+        defined __arm__                         \
+)
+#define YEP_IS_ON "arm64"
+#endif
+
+// Data types
+typedef struct
+{
+    int CHAR;
+    int INT;
+    int LONG;
+    int FLOAT;
+    int DOUBLE;
+}
+BYTE_SIZE;
+
+// About exit_status
+typedef enum { no, yes } ok_status;
+
+// Platform info
+typedef enum { linux, darwin, win32 } platform_enum;
+
+// Shells
+const enum { bash, zsh, csh, cmd, powershell } shells;
+
+typedef struct
+{
+    char *command;
+}
+_shells_;
--- playgrounds/yep/yep.h
+++ playgrounds/yep/yep.h
@@ -1,75 +1,37 @@
-#define YEP_HEADER_VERSION "0.1"
+/*
+*   Predefined macro ref:
+*       - ARCH
+*           - ARM: https://developer.arm.com/documentation/dui0774/e/Other-Compiler-specific-Features/Predefined-macros
+*       - PLATFORM
+*           - Apple macOS: https://opensource.apple.com/source/WTF/WTF-7601.1.46.42/wtf/Platform.h.auto.html
+*           // Further MSVC
+*           - Microsoft Windows: https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros?view=msvc-170
+*/
 
-#include <stdlib.h>
-
-// Unixes: unix __unix__ __unix
-// MingGW: WIN32 _WIN32 __WIN32 __WIN32__ __MINGW32__ WINNT __WINNT __WINNT__ _X86_ i386 __i386
-
-#if (   defined __GNUC__                        \
-)
-#define YEP_R_C 1
-#elif ( defined __GNUG__                        || \
-        defined __clang__                       \
-)
-#define YEP_R_CPP 1
+#if (   defined(WIN32)      \
+    || defined(_WIN32))
+    #define YEP_IS_IN "win32"
+#elif defined(__linux__)
+    #define YEP_IS_IN "linux"
+#elif __APPLE__
+    #include <Availability.h>
+    #include <AvailabilityMacros.h>
+    #include <TargetConditionals.h>
+    #if TARGET_OS_MAC
+    #define YEP_IS_IN "mac"
+    #elif TARGET_OS_IPHONE
+        #define YEP_IS_IN "ios"
+    #endif
+#elif ( defined(unix)       \
+    || defined(__unix)      \
+    || defined(__unix__)
+    #define YEP_IS_IN "unix -1"
+#else
+    #define YEP_IS_IN 0
 #endif
 
-// ~/lists/predefined_macros.json
-#if (defined __linux__)
-#define YEP_IS_IN "linux"
-#elif ( defined __APPLE__ && defined __MACH__   || \
-        defined Macintosh                       || \
-        defined macintosh                       \
-)
-#define YEP_IS_IN "darwin"
-#elif ( defined __WIN64                         || \
-        defined __WIN32                         || \
-        defined __WIN16                         || \
-        defined __WINDOWS__                     \
-)
-#define YEP_IS_IN "win32"
+#if (   defined __arm__     \
+    || defined __aarch64__)
+    #define YEP_IS_ON "arm"
+#elif (defined __)
 #endif
-
-#if (   defined __amd64__                       || \
-        defined __amd64                         || \
-        defined __x86_64__                      || \
-        defined __x86_64                        || \
-        defined i386                            || \
-        defined __i386                          || \
-        defined __i386__                        || \
-        defined _X86_                           || \
-        defined _M_IX86                         || \
-        defined _M_I86                          \
-)
-#define YEP_IS_ON "amd64"
-#elif ( defined __aarch64__                     || \
-        defined __arm__                         \
-)
-#define YEP_IS_ON "arm64"
-#endif
-
-// Data types
-typedef struct
-{
-    int CHAR;
-    int INT;
-    int LONG;
-    int FLOAT;
-    int DOUBLE;
-}
-BYTE_SIZE;
-
-// About exit_status
-typedef enum { no, yes } ok_status;
-
-// Platform info
-typedef enum { linux, darwin, win32 } platform_enum;
-
-// Shells
-const enum { bash, zsh, csh, cmd, powershell } shells;
-
-typedef struct
-{
-    char *command;
-}
-_shells_;
