9022d1040d32b0222b65baf6a39a8debf985591c
naiithink <potsawattkw@outlook.com>
2022-01-06 00:22:25 +0700

    .

--- learn/at-u/KU/Y1-02/01418113-LAB/LAB05/01.c
+++ learn/at-u/KU/Y1-02/01418113-LAB/LAB05/01.c
@@ -7,9 +7,9 @@
 int main(void)
 {
     char number_str[NUM_STR_SIZE];
-    int number = 0, prt = 0, digit_count = 0, res = 0, res_i = 0, is_automorphic = 0;
+    unsigned long long number = 0, prt = 0, digit_count = 0, res = 0, res_i = 0, is_automorphic = 0;
 
-    fputs("Enter n = ", stdout);
+    fputs("Input n = ", stdout);
     fgets(number_str, NUM_STR_SIZE, stdin);
     if ((number=atoi(number_str)) <= 0 || number > 100000000)
         return 1;
@@ -20,7 +20,7 @@ int main(void)
         return 1;
 
     { /* LOCAL */
-        int n = number;
+        long unsigned int n = number;
         while (n)
         {
             n /= 10;
@@ -28,16 +28,16 @@ int main(void)
         }
     }
 
-    int number_dig[digit_count], res_dig[digit_count];
+    long unsigned int number_dig[digit_count], res_dig[digit_count];
 
-    for (int i = 1; i < EXPO; i++)
+    for (long unsigned int i = 1; i < EXPO; i++)
     {
         res *= number;
     }
 
     res_i = res;
 
-    for (int i = 0; i < digit_count; i++)
+    for (long unsigned int i = 0; i < digit_count; i++)
     {
         if ((number_dig[i] = number%10) != (res_dig[i]=res_i%10))
         {
@@ -49,11 +49,11 @@ int main(void)
         is_automorphic = 1;
     }
 
-    printf("n^2 = %i\n", res);
+    printf("n^2 = %li\n", res);
     if (is_automorphic)
-        printf("Yes. %i is automorphic number.\n", prt);
+        printf("Yes. %li is automorphic number.\n", prt);
     else
-        printf("No. %i is not automorphic number.\n", prt);
+        printf("No. %li is not automorphic number.\n", prt);
 
     return 0;
 }
--- learn/at-u/KU/Y1-02/01418113-LAB/LAB05/02.c
+++ learn/at-u/KU/Y1-02/01418113-LAB/LAB05/02.c
@@ -2,28 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define STDIN_NUM_SIZE 10
+
 int main(void)
 {
-    char num_str[5];
-    float num = 0.0, c = 0.0, b = 0.0, a = 0.0, rem = 0.0;
-    fgets(num_str, sizeof(num_str), stdin);
-    num = atof(num_str);
+    char num_str[STDIN_NUM_SIZE];
+    double n = 0.0, c = 0.0, b = 0.0, a = 0.0, ctl = 1.0, sine = 0.0, sum = 0.0;
 
-    for (c = pow(num, 2.0); c > 0.0; c--)
+    fgets(num_str, sizeof(STDIN_NUM_SIZE), stdin);
+    n = atof(num_str);
+
+    if (n < 0.0)
+        ctl = -1.0;
+
+    for (c = 1.0; c < n; c += ctl)
     {
-        for (a = 2.0; pow(a, 2.0) < pow(c, 2.0); a++)
+        for (b = 1.0; b < c; b += ctl)
         {
-            a = pow(a, 2.0);
-            b = pow((a + b), 0.5);
-            rem = b - ((b / 2.0) * 2.0);
-            if (!rem && a < b && b < c && (num = a + b + c))
+            for (a = 1.0; a < b; a += ctl)
             {
-                printf("(%.0f, %.0f, %.0f)\n", a, b, c);
-                return 0;
+                printf(">>> (%lf, %lf, %lf)\n", a, b, c);
+                sum = a + b + c;
+                if (((double) a / (double) c) >= sin(M_PI / 3.0))
+                    continue;
+                else if (n == sum && c == (pow ((pow (a, 2.0) + pow (b, 2.0)), 0.5)))
+                {
+                    printf("(%lf, %lf, %lf)\n", a, b, c);
+                    return 0;
+                }
             }
         }
     }
     printf("No triple found.\n");
-
-    return 0;
+    
+    return 1.0;
 }
