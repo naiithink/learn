7a89a49b8e83edf40d3b12077bee29ef12916210
naiithink <potsawattkw@outlook.com>
2022-01-09 10:55:16 +0700

    update yep

---
+++ playgrounds/yep/yapp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "yep.h"
+
+static int exit_status;
+
+int
+main (int argc, char **argv)
+{
+    running_ok ok = dne;
+
+    
+
+    return exit_status = ok ? EXIT_SUCCESS : EXIT_FAILURE;
+}
