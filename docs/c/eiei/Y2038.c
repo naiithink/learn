e6b3f620a3bf43ccc0202dae6092429e8c0af337
naiithink <potsawattkw@outlook.com>
2022-01-16 15:50:59 +0700

    Y2038

--- docs/c/eiei/Y2038.c
+++ docs/c/eiei/Y2038.c
@@ -6,7 +6,7 @@ int main(void)
     time_t epoch = 0;
     time_t timestamp = epoch + 2147483647;
 
-    timestamp += 2147483647;
+    timestamp += 1;
     
     printf("%s", asctime(gmtime(&timestamp)));
     
