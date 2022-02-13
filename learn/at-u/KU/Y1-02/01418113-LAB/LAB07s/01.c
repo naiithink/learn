9eaf9ac2c8a9a009346f6bf0bac1466f7b3b405e
naiithink <potsawattkw@outlook.com>
2022-02-13 12:33:57 +0700

    .

--- learn/at-u/KU/Y1-02/01418113-LAB/LAB07s/01.c
+++ learn/at-u/KU/Y1-02/01418113-LAB/LAB07s/01.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
 
+const int str_size = 255;
+char sbuff[str_size];
+char wasfound[str_size+1];
+
 int main(void)
 {
-    // int VOWEL[] = { 97, 101, 105, 111, 117 };
-    char sbuff[255];
-    char svowl[255];
     char s = '\0';
-    int vowel[] = { 65, 69, 73, 79, 85 };
-    int found[5] = { 0 };
+    int found[255] = { 0 };
+    // int vowel[5] = { 65, 69, 73, 79, 85 };
+    // int Vowel[5] = { 97, 101, 105, 111, 117 };
+    int vowel[10] = { 65, 69, 73, 79, 85, 97, 101, 105, 111, 117 };
+    int wasfound_i = 0;
     int count = 0;
-    int k = 0;
 
     printf("String (without a space): ");
     scanf("%s", sbuff);
 
     for (int i = 0; sbuff[i] != '\0'; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < 10; j++)
         {
-            if (sbuff[i] == vowel[j] || sbuff[i] - 32 == vowel[j])
+            if (sbuff[i] == vowel[j])
             {
-                svowl[k] = sbuff[i];
-                k++;
-                if (!found[j])
-                {
-                    found[j] = 1;
-                    count++;
-                    if (count > 1)
-                        s = 's';
-                }
+                found[j] = 1;
+                wasfound[wasfound_i] = sbuff[i];
+                wasfound[++wasfound_i] = 32;
+                wasfound_i++;
+                count++;
+                if (count > 1)
+                    s = 's';
             }
         }
     }
-    svowl[k+1] = '\0';
+    wasfound[wasfound_i+1] = '\0';
 
-    for (int i = 0; svowl[i] != '\0'; i++)
+    for (int i = 0; i < str_size; i++)
     {
-        if (found[i])
-            printf("%c ", vowel[i] + 32);
+        printf("%c ", wasfound[i]);
     }
     printf("\n");
     printf("This string contains %d vowel%c.\n", count, s);
