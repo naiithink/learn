ea44967465c6d591501bff6327c1002d5dd98f09
naiithink <potsawattkw@outlook.com>
2022-02-15 22:27:04 +0700

    .

---
+++ learn/at-u/KU/Y1-02/01418113-LAB/LAB07s/03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void stringcat(char src[], char dest[])
+{
+    int len = 0;
+    int src_i = 0;
+
+    for (int i = 0; dest[i] != '\0'; i++)
+        if (dest[i+1] == '\0') \
+            len = i;
+    
+    do
+    {
+        dest[++len] = src[src_i++];
+    }
+    while (src[src_i] != '\0');
+}
+
+int main()
+{
+  char in1[100],in2[100];
+
+  printf("Input 1: ");
+  gets(in1);  /* read a line of characters from the input to "in1" variable */
+  printf("Input 2: ");
+  gets(in2);  /* read another line of characters from the input to "in2" variable */
+  stringcat(in1,in2);
+  printf(" Output: ");
+  printf("%s\n",in2);
+  return 0;
+}
