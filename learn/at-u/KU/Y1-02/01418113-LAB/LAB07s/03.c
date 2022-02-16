35a1e102bdd14a039d19fcf887b86cddcd2ac959
naiithink <potsawattkw@outlook.com>
2022-02-16 16:01:51 +0000

    01418113-LABs ipynb

--- learn/at-u/KU/Y1-02/01418113-LAB/LAB07s/03.c
+++ learn/at-u/KU/Y1-02/01418113-LAB/LAB07s/03.c
@@ -2,30 +2,36 @@
 
 void stringcat(char src[], char dest[])
 {
-    int len = 0;
-    int src_i = 0;
+    int i = 0;
+    int j = 0;
+    int begin = 0;
+    int done = 0;
 
-    for (int i = 0; dest[i] != '\0'; i++)
-        if (dest[i+1] == '\0') \
-            len = i;
-    
-    do
+    while (!done)
     {
-        dest[++len] = src[src_i++];
+        if (dest[i] == '\0')
+            begin = 1;
+        if (begin)
+        {
+            dest[i] = src[j];
+            j++;
+            if (src[j-1] == '\0')
+                done = 1;
+        }
+        i++;
     }
-    while (src[src_i] != '\0');
 }
 
 int main()
 {
-  char in1[100],in2[100];
+    char in1[100], in2[100];
 
-  printf("Input 1: ");
-  gets(in1);  /* read a line of characters from the input to "in1" variable */
-  printf("Input 2: ");
-  gets(in2);  /* read another line of characters from the input to "in2" variable */
-  stringcat(in1,in2);
-  printf(" Output: ");
-  printf("%s\n",in2);
-  return 0;
+    printf("Input 1: ");
+    gets(in1); /* read a line of characters from the input to "in1" variable */
+    printf("Input 2: ");
+    gets(in2); /* read another line of characters from the input to "in2" variable */
+    stringcat(in1, in2);
+    printf(" Output: ");
+    printf("%s\n", in2);
+    return 0;
 }
