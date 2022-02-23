bda4cb0dc9a31656d5b761f69c27383f7277462e
naiithink <potsawattkw@outlook.com>
2022-02-23 18:51:41 +0000

    .

---
+++ yep.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main (int argc, char **argv)
+{
+    char cmd[255];
+
+    printf("\n\n\n\n");
+
+    printf("\033[3F");
+    printf("yep 1.0\n\n\n");
+    sleep(1);
+
+    printf("\033[2F");
+    printf("loading\n\n");
+    
+    for (int n = 0; n < 5; n++)
+    {
+        usleep(500000);
+        printf("\033[2F");
+        printf("\033[8C");
+        printf(".\n\n");
+    
+        usleep(500000);
+        printf("\033[2F");
+        printf("\033[9C");
+        printf(".\n\n");
+
+        usleep(500000);
+        printf("\033[2F");
+        printf("\033[10C");
+        printf(".\n\n");
+        
+        usleep(500000);
+        printf("\033[2F");
+        printf("\033[8C");
+        printf("   \n\n");
+    }
+    usleep(500000);
+    printf("\033[2F");
+    printf("yep is now ready.\n\n");
+
+    printf("💩 ");
+    fgets(cmd, 255, stdin);
+
+    return 0;
+}
--- zlambda/run/in/test.c
+++ zlambda/run/in/test.c
@@ -8,47 +8,39 @@ int main(void)
 \n\
 \n");
 
-    printf("\033[3F");
+    printf("\003[3F");
     sleep(1);
 
-    printf("\033[11mHello\033[0m\n\n\n");
-    printf("\033[12mHello\033[0m\n\n\n");
-    printf("\033[13mHello\033[0m\n\n\n");
-    printf("\033[14mHello\033[0m\n\n\n");
-    printf("\033[15mHello\033[0m\n\n\n");
-    printf("\033[16mHello\033[0m\n\n\n");
-    printf("\033[17mHello\033[0m\n\n\n");
-    printf("\033[18mHello\033[0m\n\n\n");
-    printf("\033[19mHello\033[0m\n\n\n");
+    printf("\003[1mHello\003[0m\n\n\n");
     sleep(1);
 
-    printf("\033[2F");
+    printf("\003[2F");
     printf("world\n\n");
     sleep(1);
 
-    printf("\033[1F");
-    printf("\033[31m");
+    printf("\003[1F");
+    printf("\003[31m");
     printf("eiei.\n");
     sleep(1);
 
-    printf("\033[2F");
-    printf("\033[31m");
+    printf("\003[2F");
+    printf("\003[31m");
     printf("eiei.\n");
-    printf("\033[1E");
+    printf("\003[1E");
     // sleep(1);
 
-    printf("\033[1F");
-    printf("\033[32m");
+    printf("\003[1F");
+    printf("\003[32m");
     printf("ahii.\n");
     sleep(1);
 
-    printf("\033[1F");
-    printf("\033[31m");
+    printf("\003[1F");
+    printf("\003[31m");
     printf("eiei.\n");
     sleep(1);
 
-    printf("\033[1F");
-    printf("\033[32m");
+    printf("\003[1F");
+    printf("\003[32m");
     printf("ahii.\n");
 
     return 0;
