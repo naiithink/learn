e10886eed58a2465b5591887e642beeb9ff13b7e
naiithink <potsawattkw@outlook.com>
2022-03-11 07:01:07 +0000

    done reading xml

---
+++ learn/c/xml/rw.c
@@ -0,0 +1,95 @@
+#include <libxml2/libxml/parser.h>
+#include <libxml2/libxml/xmlstring.h>
+#include <libxml2/libxml/tree.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum { false, true } ProgStatus;
+
+static int exit_status;
+
+void
+readXML (const xmlDocPtr docPtr, const xmlNodePtr nodePtr);
+
+int
+main (int argc, char **argv)
+{
+    ProgStatus ok = true;
+
+    if (argc == 1)
+    {
+        fprintf (stderr, "No input file.\n");
+        ok = false;
+    }
+
+    if (ok)
+    {
+        const char *filename = argv[1];
+        char *value;
+        xmlDocPtr doc_p;
+        xmlNodePtr node_p;
+
+        doc_p = xmlParseFile (filename);
+        node_p = xmlDocGetRootElement (doc_p);
+
+        readXML (doc_p, node_p);
+    }
+
+    return exit_status = ok ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+void
+readXML (const xmlDocPtr docPtr, const xmlNodePtr nodePtr)
+{
+    char *check = "done.";
+    char *name;
+    xmlChar *value;
+    xmlDocPtr doc_p = docPtr;
+    const xmlNodePtr root_p = nodePtr;
+    xmlNodePtr node_p = nodePtr;
+    xmlNodePtr parent_p;
+
+    while (node_p != NULL)
+    {
+        if (node_p->children != NULL)
+        {
+            parent_p = node_p;
+            name = (char *) parent_p->name;
+            node_p = node_p->children;
+            value = xmlNodeListGetString (doc_p, node_p, 1);
+
+            if (parent_p == root_p)
+            {
+                printf("%s :\n", name);
+            }
+            else if (strcmp (name, "word"))
+            {
+                printf("\t%s \t-> ", name);
+            }
+            else
+            {
+                printf("\t%s \t-> \t%s", name, (char *) value);
+                if (!strcmp (name, "word"))
+                {
+                    printf("\n");
+                }
+            }
+
+
+            // value = (xmlChar *) check;
+            strcpy ((char *) value, check);
+        }
+        else if (node_p->next != NULL)
+        {
+            node_p = node_p->next;
+        }
+        else if (node_p->next == NULL)
+        {
+            node_p = node_p->parent;
+            node_p = node_p->next;
+        }
+    }
+
+    // xmlFree (value);
+}
