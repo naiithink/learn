7744c21cad766cec1edccc963d1ecd5831a2d7da
naiithink <potsawattkw@outlook.com>
2022-03-14 06:38:32 +0700

    big org

--- learn/atu/ku/Y1-01/01418112-LEC/FinalAssignment/6410451199-12.py
+++
@@ -1,8 +0,0 @@
-iPut = input()
-excact = len(iPut)
-remain = len(iPut)
-while remain:
-    toPrint = " " * (excact - remain) + " ".join(iPut)
-    print(toPrint)
-    remain -= 1
-    iPut = iPut[:remain]
