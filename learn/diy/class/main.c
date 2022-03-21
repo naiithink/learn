f3bbc42a439fea4ed4871524423776977885ad73
naiithink <potsawattkw@outlook.com>
2022-03-21 21:30:35 +0700

    .

---
+++ learn/diy/class/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+/* #include "modman.h"
+
+int main(void)
+{
+    modman
+
+    return 0;
+} */
+
+typedef struct pstring_t {
+    char * chars;
+    int(*length)(void*);
+} PString;
+
+int length(void* self) {
+    return strlen(((PString*)self)->chars);
+}
+
+PString initializeString() {
+    PString str;
+    str.length = &length;
+    return str;
+}
+
+int main()
+{
+    PString p = initializeString();
+
+    p.chars = "Hello";
+
+    printf("Length: %i\n", p.length(&p));
+
+    return 0;
+}
