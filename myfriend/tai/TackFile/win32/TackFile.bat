e05047c4bef8476f404b3ee72b303add2bbc4094
naiithink <potsawattkw@outlook.com>
2022-05-03 17:47:49 +0700

    cmd

--- myfriend/tai/TackFile/win32/TackFile.bat
+++ myfriend/tai/TackFile/win32/TackFile.bat
@@ -1,5 +1,32 @@
-@echo off
-chcp 65001 >NUL
-set /p WORD=ใส่ข้อความ:
-echo %WORD%
-pause
+@ECHO OFF
+
+SET LOGFILE="%~pn0.log"
+SET ARFILES="pritai.tar" "pritai-dad.tar"
+SET OUTDIR="%~pn0_out"
+
+IF NOT EXIST %OUTDIR% (
+    ECHO %DATE% %TIME% Running MKDIR ... >> %LOGFILE%
+    MKDIR %OUTDIR% >NUL 2>&1
+    IF ERRORLEVEL 1 (
+        ECHO Error: Cannot create an output folder, stopped. 1>&2
+        EXIT 1
+    ) ELSE (
+        ECHO Extracting to the folder: %OUTDIR%
+    )
+)
+
+FOR %%F IN (%ARFILES%) DO (
+    IF EXIST %%F (
+        tar -xf %%F -C %OUTDIR% >NUL 2>&1
+        IF ERRORLEVEL 1 (
+            ECHO Error: Cannot extract %%F, stopped. 1>&2
+            EXIT 1
+        )
+    )
+)
+
+FOR %%F IN 
+
+ECHO Done, eiei.
+
+PAUSE
