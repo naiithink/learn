edc92c753eb72061051c028e898a48dbb081c7e9
naiithink <potsawattkw@outlook.com>
2022-05-31 04:14:34 +0700

    try: Complicated C syntax

--- test.c
+++ test.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef void *(*func_exp)(void *);
+typedef void ((*func_exp_a)(void *))[];
+
+int _n(int x)
+{
+    return ++x;
+}
 
 int main(void)
 {
-    int n = 10 /*/*/*10/**/;
+    func_exp x; // malloc(sizeof(func_exp *));
+    func_exp_a n; // malloc(sizeof(func_exp *));
 
-    printf("%i\n", n);
+    x = &_n;
+    n = { (*x)(1) };
+
+    printf("%zu\n", sizeof(func_exp));
 
     return 0;
 }
