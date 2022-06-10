362943e3851d1be23e4369948664b278507f4783
naiithink <potsawattkw@outlook.com>
2022-06-10 18:25:59 +0700

    backup

--- test.c
+++ test.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef void *(*func_exp)(void *);
-typedef void ((*func_exp_a)(void *))[];
-
-int _n(int x)
-{
-    return ++x;
-}
+// typedef void *(*func_exp)(void *);
+// typedef void ((*func_exp_a)(void *))[];
+// 
+// int _n(int x)
+// {
+//     return ++x;
+// }
 
 int main(void)
 {
-    func_exp x; // malloc(sizeof(func_exp *));
-    func_exp_a n; // malloc(sizeof(func_exp *));
+    long *p = malloc(sizeof *p);
 
-    x = &_n;
-    n = { (*x)(1) };
+    *p = 10;
 
-    printf("%zu\n", sizeof(func_exp));
+    printf("%ld\n", *p);
+    // func_exp x; // malloc(sizeof(func_exp *));
+    // func_exp_a n; // malloc(sizeof(func_exp *));
+
+    // x = &_n;
+    // n = { (*x)(1) };
+
+    // printf("%zu\n", sizeof(func_exp));
 
     return 0;
 }
