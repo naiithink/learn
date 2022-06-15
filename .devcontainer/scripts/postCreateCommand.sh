039e77f54ff605a964e7fe6a21c6b081c8d8176d
naiithink <potsawattkw@outlook.com>
2022-06-15 17:25:37 +0000

    feat(devcontainer): Updates

---
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+SHELL='zsh'
+PROJ='private'
+USER_NAME='naiithink'
+USER_EMAIL='potsawattkw@outlook.com'
+
+echo "export SHELL=$SHELL" >> $HOME/.zshrc
+echo 'export GPG_TTY=$(tty)' >> $HOME/.zshrc
+git config --global oh-my-zsh.hide-info 1
+git config --global core.autocrlf input
+git config --global user.name $USER_NAME
+git config --global user.email $USER_EMAIL
+git config --global commit.template .gitmessage
+git remote add gh git@github.com:${USER_NAME}/${PROJ}.git
+git remote add all git@gitlab.com:${USER_NAME}/${PROJ}.git
+git remote set-url --push --add all git@gitlab.com:${USER_NAME}/${PROJ}.git
+git remote set-url --push --add all git@github.com:${USER_NAME}/${PROJ}.git
