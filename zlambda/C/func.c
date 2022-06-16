5229304f131407934da0908bada4cf981d15e4d7
naiithink <potsawattkw@outlook.com>
2022-06-16 17:00:22 +0700

    update

--- zlambda/C/func.c
+++ zlambda/C/func.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void say_hello(void)
+{
+    printf("hello, world\n");
+}
+
+void say_hi(void)
+{
+    printf("hi, world\n");
+}
+
+void invoke(void (*f[])(void), int n_func)
+{
+    for (int i = 0; i < n_func; ++i)
+        (*f[i])();
+}
+
 int main(void)
 {
-    
+    void (*f[])(void) = { &say_hello, &say_hi };
+    invoke(f, 2);
+
+    void (*fp)(void) = &say_hello;
+
+    (*fp)();
 
     return 0;
 }
