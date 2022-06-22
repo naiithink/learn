9bb3c0305514dfafd7ea855f26e35074f0cb48a6
Potsawat <potsawattkw@outlook.com>
2022-06-22 13:20:20 +0000

    feat(devcontainer): Configs

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-SHELL='zsh'
 PROJ='private'
-USER_NAME='Potsawat Thinkanwatthana'
-USER_EMAIL='potsawattkw@outlook.com'
+# USER_NAME='Potsawat Thinkanwatthana'
+# USER_EMAIL='potsawattkw@outlook.com'
 
-echo "export SHELL=$SHELL" >> $HOME/.${SHELL}rc
-git config --global oh-my-zsh.hide-info 1
-git config --global core.autocrlf input
-git config --global user.name $USER_NAME
-git config --global user.email $USER_EMAIL
-git config --global commit.template .gitmessage
-git remote add gh git@github.com:${USER_NAME}/${PROJ}.git
-git remote add all git@gitlab.com:${USER_NAME}/${PROJ}.git
-git remote set-url --push --add all git@gitlab.com:${USER_NAME}/${PROJ}.git
-git remote set-url --push --add all git@github.com:${USER_NAME}/${PROJ}.git
+# git config --global oh-my-zsh.hide-info 1
+# git config --global core.autocrlf input
+# git config --global user.name $USER_NAME
+# git config --global user.email $USER_EMAIL
+# git config --global commit.template .gitmessage
+# git remote add gh git@github.com:${USER_NAME}/${PROJ}.git
+# git remote add all git@gitlab.com:${USER_NAME}/${PROJ}.git
+# git remote set-url --push --add all git@gitlab.com:${USER_NAME}/${PROJ}.git
+# git remote set-url --push --add all git@github.com:${USER_NAME}/${PROJ}.git
