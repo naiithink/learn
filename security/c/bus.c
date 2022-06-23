1f1362b2cd6b3ca8c0e2faefb39e6c218c3bc4d5
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-06-23 21:08:08 +0700

    Updates

--- security/c/bus.c
+++ security/c/bus.c
@@ -1,105 +1,117 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 int sigbusn;
 int sigsegvn;
 
 struct list
 {
-	unsigned m:4;
-	unsigned n:3;
+    unsigned m:4;
+    unsigned n:3;
 }__attribute__((packed));
 
 struct nib
 {
-	int i:4;
+    // unsigned i:4;
+    uint8_t i:4;
 }__attribute__((packed));
 
 void got_bus(int signo)
 {
-	if (!sigbusn)
-		sigbusn = 1;
-	else
-		// return;
-		abort();
+    if (!sigbusn)
+        sigbusn = 1;
+    else
+        // return;
+        abort();
 
-	fprintf(stderr, "\nGOT BUS\n\n");
-	// exit(10);
+    printf("\nGOT BUS\n\n");
+    exit(101);
 }
 
 void got_segv(int signo)
 {
-	if (!sigsegvn)
-		sigsegvn = 1;
-	else
-		// return;
-		abort();
+    if (!sigsegvn)
+        sigsegvn = 1;
+    else
+        // return;
+        abort();
 
-	fprintf(stderr, "\nGOT SEGV\n\n");
-	// exit(9);
+    printf("\nGOT SEGV\n\n");
+    exit(102);
 }
 
 int main(void)
 {
-	char *pc = NULL;
-	char *pcls = NULL;
-	char *pcnib = NULL;
-	char *pcpnb = NULL;
-	int m = 10;
-	int n = 15;
-	int *pm = &m;
-	int *pn = &n;
-	struct list ls = { .m = 15, .n = 3 };
-	struct list *pls = &ls;
+    char *pc = NULL;
+    char *pcls = NULL;
+    char *pcnib = NULL;
+    char *pcpnb = NULL;
+    int m = 10;
+    int n = 15;
+    int *pm = &m;
+    int *pn = &n;
+    struct list ls = { .m = 15, .n = 3 };
+    struct list *pls = &ls;
 
-	signal(SIGBUS, &got_bus);
-	signal(SIGSEGV, &got_segv);
-	// signal(SIGBUS, printf(">>> got bus\n"));
-	// signal(SIGSEGV, printf(">>> got segv\n"));
+    signal(SIGBUS, &got_bus);
+    signal(SIGSEGV, &got_segv);
 
-	struct nib *pnb1 = malloc(sizeof *pnb1);
-	struct nib *pnb2 = malloc(sizeof *pnb2);
-	struct nib *pnb3 = malloc(sizeof *pnb3);
+    // kill(getpid(), SIGBUS);
+    // kill(getpid(), SIGSEGV);
 
-	pnb1->i = 7;
-	pnb2->i = 0;
-	pnb3->i = 7;
+    struct nib *pnb1 = malloc(sizeof *pnb1);
+    struct nib *pnb2 = malloc(sizeof *pnb2);
+    struct nib *pnb3 = malloc(sizeof *pnb3);
 
-	pcpnb = (char *) pnb1 + 1;
+    pnb1->i = 15;
+    pnb2->i = 0;
+    pnb3->i = 15;
 
-	printf("n(struct nib): 	%lu\n", sizeof(struct nib));
-	printf("pcnib: 		%p: %i\n", pcnib, *pcnib);
+    pcpnb = (char *) pnb1 + 1;
 
-	struct nib a = { .i = 7 };
-	struct nib b = { .i = 0 };
-	struct nib c = { .i = 7 };
-	struct nib *pnib = &a;
+    printf("n(*pnb1):           %lu\n", sizeof *pnb1);
+    printf("n(struct nib):      %lu\n", sizeof(struct nib));
+    printf("pcnib:              %14p:    % 03i\n", pcnib, *pcnib);
 
-	pcnib = (char *) pnib + 1;
+    free(pnb1);
+    free(pnb2);
+    free(pnb3);
 
-	printf("n(struct nib): 	%lu\n", sizeof(struct nib));
-	printf("pcnib: 		%p: %i\n", pcnib, *pcnib);
+    // exit(11);
 
-	printf("n(m):  			%lu\n", sizeof m);
-	printf("n(ls): 			%lu\n", sizeof ls);
+    struct nib a = { .i = 15 };
+    struct nib b = { .i = 0 };
+    struct nib c = { .i = 15 };
+    struct nib *pnib = &a;
 
-	pc = (char *) pm + 1;
-	pcls = (char *) pls + 1;
-	printf("> pcls: 	%p: %i\n", pcls, *pcls);
+    pcnib = (char *) pnib + 1;
 
-	pcls = (char *) pls + 1;
-	printf("> pcls: 	%p: %i\n", pcls, *pcls);
+    printf("n(struct nib):      %lu\n", sizeof(struct nib));
+    printf("pcnib:              %14p:    % 03i\n", pcnib, *pcnib);
 
-	printf("pm: 		%p: %i\n", pm, *pm);
-	printf("pn: 		%p: %i\n", pn, *pn);
-	printf("pc: 		%p: %i\n", pc, *pc);
-	printf("pcls: 		%p: %i\n", pcls, *pcls);
+    printf("n(m):               %lu\n", sizeof m);
+    printf("n(ls):              %lu\n", sizeof ls);
 
-	printf("gotcha\n");
+    // exit(12);
 
-	free(pnb1);
-	free(pnb2);
-	free(pnb3);
-	return 0;
+    pc = (char *) pm + 1;
+    pcls = (char *) pls + 1;
+    printf("> pcls:             %14p:    % 03i\n", pcls, *pcls);
+
+    pcls = (char *) pls + 1;
+    printf("> pcls:             %14p:    % 03i\n", pcls, *pcls);
+
+    printf("pm:                 %14p:    % 03i\n", pm, *pm);
+    printf("pn:                 %14p:    % 03i\n", pn, *pn);
+    printf("pc:                 %14p:    % 03i\n", pc, *pc);
+    printf("pcls:               %14p:    % 03i\n", pcls, *pcls);
+
+    // exit(13);
+
+    printf("gotcha\n");
+
+    return 0;
 }
