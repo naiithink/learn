2d5c8a44803cacee58195cd8fd4849bd8cbb7a85
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-07-04 23:26:45 +0700

    learn: Java stderr

---
+++ learn/java/stderr/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Color tooRed = new Color("tooRed", 256, 0, 0);
+        Color red = new Color("red", 255, 0, 0);
+
+        System.out.println(tooRed);
+        System.out.println(red);
+    }
+}
