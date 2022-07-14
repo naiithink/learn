47dba673cf8448a8d809010f824155daf253929c
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-07-14 12:04:13 +0700

    feat: Initial commit

---
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# PROJ='private'
+# USER_NAME='Potsawat Thinkanwatthana'
+# USER_EMAIL='potsawattkw@outlook.com'
+
+git config oh-my-zsh.hide-info 1
+# git config --global core.autocrlf input
+# git config --global user.name $USER_NAME
+# git config --global user.email $USER_EMAIL
+# git config --global commit.template .gitmessage
+# git remote add gh git@github.com:${USER_NAME}/${PROJ}.git
+# git remote add all git@gitlab.com:${USER_NAME}/${PROJ}.git
+# git remote set-url --push --add all git@gitlab.com:${USER_NAME}/${PROJ}.git
+# git remote set-url --push --add all git@github.com:${USER_NAME}/${PROJ}.git
+
+exit 0
