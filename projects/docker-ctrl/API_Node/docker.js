e48c827e3e28d4d7994b9e11bf46385ff1d03b75
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-07-19 21:51:15 +0700

    feat: Add remove submodule guide

--- projects/docker-ctrl/API_Node/docker.js
+++
@@ -1,9 +0,0 @@
-const Docker = require('dockerode')
-
-const docker = new Docker({socketPath: '/var/run/docker.sock'});
-
-docker.listContainers({all: true}, function(err, containers) {
-    containers.forEach((data) => {
-        console.log(data);
-    })
-});
--- projects/docker-ctrl/config.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-export DOCKER_HOST="tcp://0.0.0.0:2375"
-docker ps
